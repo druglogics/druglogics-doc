8109a02173c7688caab202804c27064c607d738e
john <bblodfon@gmail.com>
2020-04-01 19:49:01 +0200

    add travis and deploy script

---
+++ _deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+#git config --global user.email "bblodfon@gmail.com"
+#git config --global user.name "john"
+
+# TRAVIS_REPO_SLUG = owner_name/repo_name
+git clone https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git repo
+cd repo
+git add docs/*
+git commit -m "update docs" || true
+git push
+
